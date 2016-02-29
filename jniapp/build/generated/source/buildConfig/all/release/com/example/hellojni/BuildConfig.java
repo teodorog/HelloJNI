9e1d8ca6c9e29afff6e2f1b36506e10862b27dea
Teodoro Guadalupi <Teodoro Guadalupi>
2016-02-29 23:16:21 +0100

    Initial commit

---
+++ jniapp/build/generated/source/buildConfig/all/release/com/example/hellojni/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.hellojni;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.hellojni";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "all";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
