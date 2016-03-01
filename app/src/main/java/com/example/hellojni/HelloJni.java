6836182df601409bd2fa1ff795471ebb9d9a47ce
teodoroguadalupi@gmail.com <peluso15>
2016-03-02 00:14:45 +0100

    riuscito salvataggio immagine

--- app/src/main/java/com/example/hellojni/HelloJni.java
+++ app/src/main/java/com/example/hellojni/HelloJni.java
@@ -25,110 +25,107 @@ import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class HelloJni extends Activity
-{
+public class HelloJni extends Activity {
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private Uri fileUri;
+    private static File myfile;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        /* Create a TextView and set its content.
-         * the text is retrieved by calling a native
-         * function.
-         */
-        TextView  tv = new TextView(this);
-        tv.setText( stringFromJNI() );
-        setContentView(tv);
-
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        //fileUri = getOutputMediaFileUri(); // create a file to save the image
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-        // start the image capture Intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    /**
+     * Crea un file Uri per salvare un' immagine
+     */
+    private static Uri getOutputMediaFileUri() {
+        myfile = getOutputMediaFile();
+        return Uri.fromFile(myfile);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
-        {
-        if (resultCode == RESULT_OK)
-            {
-            // Image captured and saved to fileUri specified in the Intent
-            Toast.makeText(this, "Image saved to:\n" + data.getData(), Toast.LENGTH_LONG).show();
-            } else if (resultCode == RESULT_CANCELED)
-            {
-            // User cancelled the image capture
-            } else
-            {
-            // Image capture failed, advise user
-            }
+    /**
+     * Crea un File per salvare un' immagine
+     */
+    private static File getOutputMediaFile() {
+
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+            return null;
         }
-    }
 
-    /** Create a file Uri for saving an image or video */
-    private static Uri getOutputMediaFileUri(){
-        return Uri.fromFile(getOutputMediaFile());
-    }
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "hellojni");
+        // questa locazione lavora meglio se vuoi creare un' immagine che deve essere condivisa
+        // tra applicazioni
 
-    /** Create a File for saving an image or video */
-    private static File getOutputMediaFile(){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
+        // Crea una directory se non esiste
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("hellojni", "failed to create directory");
                 return null;
             }
         }
 
-        // Create a media file name
+        // Crea il nome di un file immagine
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + timeStamp + ".jpg");
 
         return mediaFile;
     }
 
-    /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Crea una TextView e setta il suo contenuto.
+         * il testo è recuperato chiamando una funzione nativa.
+         */
+        TextView tv = new TextView(this);
+        tv.setText(stringFromJNI());
+        setContentView(tv);
+
+        // crea un Intent perottenre un' immagine
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(); // crea un file per salvarel' immagine
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set il nome del file immagine
+
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // immagine catturata e salvata nel fileUri specificato dall' Intent
+                Toast.makeText(this, "immagine salvata in\n" + myfile.toString(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // l' utente ha cancellato l' immagine catturata
+            } else {
+                // cattura dell' immagine fallita
+            }
+        }
+    }
+
+
+    /* A native method that is implemented by the  'hello-jni' native library, which is packaged
      * with this application.
      */
-    public native String  stringFromJNI();
+    public native String stringFromJNI();
 
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
+    /* This is another native method declaration that is *not* implemented by 'hello-jni'.
+     This is simply to show that you can declare as many native methods in your Java code
+     * as you want, their implementation is searched in the currently loaded native libraries only the first time
      * you call them.
      *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
+     * Trying to call this function will result in a java.lang.UnsatisfiedLinkError exception !
      */
-    public native String  unimplementedStringFromJNI();
+    public native String unimplementedStringFromJNI();
 
-    /* this is used to load the 'hello-jni' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.example.hellojni/lib/libhello-jni.so at
+    /* this is used to load the 'hello-jni' library on application startup.
+       The library has already been unpacked into /data/data/com.example.hellojni/lib/libhello-jni.so at
      * installation time by the package manager.
      */
     static {
